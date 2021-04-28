800e144f931327bddcdb759c2b0e7aeb1e66c80b
hideyasu2015 <hideyasu.tokuhara@gmail.com>
2021-04-28 07:16:50 +0700

    modify

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
 $(document).ready(function () {
-  
+
   $('.counter').counterUp({
     delay: 10,
     time: 1000
   });
 
+  function call_carousel() {
+    window.setInterval(AOS.refresh(), 5000);
+  }
+
+  var call_carousel = window.setInterval(call_carousel, 5000);
+
+
+  // $('#myCarousel').on('slid.bs.carousel', function () {
+  //   console.log('called');
+  //   AOS.init();
+  // })
+
+  $('#myCarousel').on('slid.bs.carousel', function () {
+    console.log('called');
+    var call_carousel = window.setInterval(AOS.refresh(), 5000);
+  });
 });
